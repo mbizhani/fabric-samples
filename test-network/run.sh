449b5f71f215a4ffa82eb3ff8d2fb6b03efcb7ec
mbizhani <mbizhani@gmail.com>
2021-01-11 15:11:22 +0330

    - add deployCC

--- test-network/run.sh
+++ test-network/run.sh
@@ -1,1 +1,7 @@
 ./network.sh up createChannel -ca -verbose
+
+echo "##########################"
+echo "####### CHAIN CODE #######"
+echo "##########################"
+
+./network.sh deployCC -ccn basic -ccp ../asset-transfer-basic/chaincode-javascript -ccl javascript
