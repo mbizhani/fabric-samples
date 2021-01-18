8ce8e97c95f3cd3ca751b1925fe1b711b57f68e6
Mehdi Bizhani <mbizhani@gmail.com>
2021-01-19 00:19:22 +0330

    external chaincode

--- test-network/run.sh
+++ test-network/run.sh
@@ -1,7 +1,99 @@
-./network.sh up createChannel
+#!/bin/bash
+
+#./network.sh up createChannel
 
 echo "##########################"
 echo "####### CHAIN CODE #######"
 echo "##########################"
 
-./network.sh deployCC -ccn basic -ccp ../asset-transfer-basic/chaincode-go -ccl go
+## Internal
+#./network.sh deployCC -ccn basic -ccp ../asset-transfer-basic/chaincode-go -ccl go
+
+## External
+pushd ../asset-transfer-basic/chaincode-external
+tar cvfz code.tar.gz connection.json
+tar cvfz asset-transfer-basic-external.tgz metadata.json code.tar.gz
+
+#docker build -t hyperledger/asset-transfer-basic .
+popd
+
+export PATH=${PWD}/../bin:$PATH
+export FABRIC_CFG_PATH=$PWD/../config/
+source ./scripts/envVar.sh
+CC_NAME="basic"
+CC_VERSION="1.0"
+
+setGlobals 1
+peer lifecycle chaincode install \
+  ../asset-transfer-basic/chaincode-external/asset-transfer-basic-external.tgz
+echo "Install 1: $?"
+
+setGlobals 2
+peer lifecycle chaincode install \
+  ../asset-transfer-basic/chaincode-external/asset-transfer-basic-external.tgz
+echo "Install 2: $?"
+
+setGlobals 1
+peer lifecycle chaincode queryinstalled \
+  --peerAddresses localhost:7051 \
+  --tlsRootCertFiles $PWD/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt >&log.txt
+cat log.txt
+PACKAGE_ID=$(sed -n "/${CC_NAME}_${CC_VERSION}/{s/^Package ID: //; s/, Label:.*$//; p;}" log.txt)
+echo "PACKAGE_ID = ${PACKAGE_ID}"
+if [ ! "${PACKAGE_ID}" ]; then
+  echo "ERROR: no PACKAGE_ID"
+  exit 1
+fi
+
+EXT_CC_ADDR="asset-transfer-basic.org1.example.com"
+
+echo "CHAINCODE_SERVER_ADDRESS=${EXT_CC_ADDR}:9999" > cc.env
+echo "CHAINCODE_ID=${PACKAGE_ID}" >> cc.env
+
+docker run -d --rm \
+  --name ${EXT_CC_ADDR} \
+  --hostname ${EXT_CC_ADDR} \
+  --env-file cc.env \
+  --network=net_test hyperledger/asset-transfer-basic
+
+sleep 2
+
+setGlobals 1
+peer lifecycle chaincode approveformyorg \
+  -o localhost:7050 \
+  --ordererTLSHostnameOverride orderer.example.com \
+  --tls --cafile $PWD/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem \
+  --channelID mychannel \
+  --name ${CC_NAME} \
+  --version ${CC_VERSION} \
+  --package-id "${PACKAGE_ID}" \
+  --sequence 1
+echo "Approve 1: $?"
+
+sleep 2
+setGlobals 2
+peer lifecycle chaincode approveformyorg \
+  -o localhost:7050 \
+  --ordererTLSHostnameOverride orderer.example.com \
+  --tls --cafile $PWD/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem \
+  --channelID mychannel \
+  --name ${CC_NAME} \
+  --version ${CC_VERSION} \
+  --package-id "${PACKAGE_ID}" \
+  --sequence 1
+echo "Approve 2: $?"
+
+sleep 2
+peer lifecycle chaincode commit \
+  -o localhost:7050 \
+  --ordererTLSHostnameOverride orderer.example.com \
+  --tls --cafile $PWD/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem \
+  --channelID mychannel \
+  --name ${CC_NAME} \
+  --version ${CC_VERSION} \
+  --peerAddresses localhost:7051 \
+  --tlsRootCertFiles $PWD/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt \
+  --peerAddresses localhost:9051 \
+  --tlsRootCertFiles $PWD/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt \
+  --sequence 1
+echo "Commit: $?"
